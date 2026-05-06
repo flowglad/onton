008b96370925584ce47914db44e33f72e0093891
Zach <zach.smith@flowglad.com>
2026-05-06 15:24:08 -0400

    Use protected findings registry mutex

--- lib/findings_registry.ml
+++ lib/findings_registry.ml
@@ -9,10 +9,7 @@ type entry = {
 type t = { mutex : Eio.Mutex.t; table : (string, entry) Hashtbl.t }
 
 let create () = { mutex = Eio.Mutex.create (); table = Hashtbl.create 64 }
-
-let with_lock t f =
-  Eio.Mutex.lock t.mutex;
-  Fun.protect ~finally:(fun () -> Eio.Mutex.unlock t.mutex) f
+let with_lock t f = Eio.Mutex.use_rw ~protect:true t.mutex f
 
 let make_key ~backend_name ~owner ~repo ~pr_number ~finding_id =
   Printf.sprintf "%s/%s/%s#%d/%s" backend_name owner repo pr_number finding_id
