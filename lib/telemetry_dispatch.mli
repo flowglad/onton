7f46be0e7d29c1d87b5b1ba96740d47865281826
Zach Smith <zd@zdsmith.com>
2026-05-19 22:25:54 +0200

    [replay-ready-diagnostics] Patch 2: Add Telemetry core (typed Event variant + Sink interface + pure router + registry) (#272)
    * Add telemetry core
    
    * Split telemetry dispatch from core
    
    * Make scoped telemetry sink restore exact registry
    
    * Remove open from telemetry dispatch interface
    
    * Remove open from telemetry core interface

---
+++ lib/telemetry_dispatch.mli
@@ -0,0 +1,4 @@
+val register_sink : Telemetry.Sink.t -> unit
+val unregister_sink : name:string -> unit
+val emit : Telemetry.Event.t -> unit
+val with_sink : sink:Telemetry.Sink.t -> (unit -> 'a) -> 'a
---
+++ lib_core/telemetry.mli
@@ -0,0 +1,52 @@
+module Event : sig
+  type level = Trace | Debug | Info | Warning | Error
+  [@@deriving show, eq, yojson]
+
+  type t =
+    | Poll of { patch_id : Types.Patch_id.t; payload : Yojson.Safe.t }
+    | Action of {
+        patch_id : Types.Patch_id.t;
+        session_uuid : string option;
+        payload : Yojson.Safe.t;
+      }
+    | Complete of {
+        patch_id : Types.Patch_id.t;
+        session_uuid : string option;
+        subkind : Failure_subkind.t;
+        payload : Yojson.Safe.t;
+      }
+    | Stream of {
+        patch_id : Types.Patch_id.t;
+        session_uuid : string;
+        channel : [ `Stdout | `Stderr ];
+        raw : string;
+      }
+    | Spawn_started of {
+        patch_id : Types.Patch_id.t;
+        session_uuid : string;
+        prompt : string;
+        argv : string list;
+        env_redacted : string array;
+      }
+    | Spawn_finalized of {
+        patch_id : Types.Patch_id.t;
+        session_uuid : string;
+        meta : Yojson.Safe.t;
+      }
+    | Free_form of {
+        patch_id : Types.Patch_id.t option;
+        level : level;
+        message : string;
+      }
+  [@@deriving show, yojson]
+end
+
+module Sink : sig
+  type t = {
+    name : string;
+    interested_in : Event.t -> bool;
+    consume : Event.t -> unit;
+  }
+end
+
+val route : sinks:Sink.t list -> Event.t -> Sink.t list
