9f88365b11ab2176b96099e6bbb2083ed9766961
Zach Smith <zach.smith@flowglad.com>
2026-05-20 11:51:30 -0700

    Extract TUI state bundle

---
+++ lib/tui_state.mli
@@ -0,0 +1,18 @@
+type t = {
+  list_selected : int ref;
+  detail_scroll : int ref;
+  detail_follow : bool ref;
+  timeline_scroll : int ref;
+  view_mode : Tui.view_mode ref;
+  sorted_patch_ids : Types.Patch_id.t list ref;
+  input_mode : Tui_input.input_mode ref;
+  prompt_line : Tui.prompt_info option ref;
+  show_help : bool ref;
+  status_msg : Tui.status_msg option ref;
+  patches_start_row : int ref;
+  patches_scroll_offset : int ref;
+  patches_visible_count : int ref;
+  detail_scrolls : (Types.Patch_id.t, int * bool) Stdlib.Hashtbl.t;
+}
+
+val create : unit -> t
